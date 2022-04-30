753b9257621189740d2592e9e3842bd4d0f54719
VictoriaRodriguez22 <104107269+VictoriaRodriguez22@users.noreply.github.com>
2022-04-30 19:33:30 -0300

    tarea 2 autos de carrera

--- programacion/clase25deabril-daniel/tarea1.ts
+++ programacion/clase25deabril-daniel/tarea1.ts
@@ -1,22 +1,32 @@
-let dato1 = document.getElementById("dato1")
-let dato2 = document.getElementById("dato2")
+let dato1 = document.getElementById("dato1");
+let dato2 = document.getElementById("dato2");
+let dato3 = document.getElementById("dato3");
+let dato4 = document.getElementById("dato4");
 
 let btnEnv = document.getElementById ("btnEnviar");
 let rotulo1 = document.getElementById ("rotulo1");
 let rotulo2 = document.getElementById ("rotulo2");
+let rotulo1 = document.getElementById ("rotulo3");
+let rotulo2 = document.getElementById ("rotulo4");
 
-rotulo1?.innerHTML = "Ingrese precio del producto";
 
-rotulo2?.innerHTML = "Ingrese descuento a aplicar";
-
+rotulo1?.innerHTML = "Tiempo de la Vuelta Número 1";
+rotulo2?.innerHTML = "Tiempo de la Vuelta Número 2";
+rotulo3?.innerHTML = "Tiempo de la Vuelta Número 3";
+rotulo4?.innerHTML = "Tiempo de la Vuelta Número 4";
 
 btnEnv?.addEventListener ("click", () => {
-let precio: number = Number(dato1.value);
-let porcentaje: number = Number(dato2.value);
+let vuelta1: number = Number(dato1.value);
+let vuelta2: number = Number(dato2.value);
+let vuelta3: number = Number(dato3.value);
+let vuelta4: number = Number(dato4.value);
 
-let descuento: number = precio*porcentaje/100;
-let preciofinal: number = precio - descuento;
-console.log("Su compra es de $" preciofinal);
+let tiempototal: number = vuelta1 + vuelta2 + vuelta3 + vuelta4;
+
+let promediodevuelta: number = tiempototal/4;
+
+console.log("El tiempo total de las vueltas es" tiempototal);
+console.log("El promedio total de vueltas es de" promediodevuelta);
 
 });
 
