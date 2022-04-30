279575d2a5951cbc91dc2ead43fc2442a076b51e
VictoriaRodriguez22 <104107269+VictoriaRodriguez22@users.noreply.github.com>
2022-04-30 17:38:36 -0300

    Initial commit

---
+++ programacion/clase22deabril-karen/baseporaltura.ts
@@ -0,0 +1,14 @@
+//ejercicio 1
+let paso1: string = "Este es mi paso";
+let paso2: string = "Este es mi segundo paso";
+let titulo: string = "Pasos para subir un posteo en facebook";
+console.log(titulo);
+console.log(paso1);
+console.log(paso2);
+
+//ejercicio 2
+let base: number = 5;
+let altura: number = 10;
+let result: number = base * altura;
+console.log(result);
+console.log(typeof result);
