522a76e29f6a7ee2de7aba07a4c966f91a93f875
VictoriaRodriguez22 <104107269+VictoriaRodriguez22@users.noreply.github.com>
2022-04-30 19:43:47 -0300

    hice tarea 2 en 1 tuve que volver a hacer la tarea 1 porque me confundí de carpeta donde hice el código

--- programacion/clase25deabril-daniel/tarea1.ts
+++ programacion/clase25deabril-daniel/tarea1.ts
@@ -1,32 +1,28 @@
 let dato1 = document.getElementById("dato1");
 let dato2 = document.getElementById("dato2");
-let dato3 = document.getElementById("dato3");
-let dato4 = document.getElementById("dato4");
+
 
 let btnEnv = document.getElementById ("btnEnviar");
 let rotulo1 = document.getElementById ("rotulo1");
 let rotulo2 = document.getElementById ("rotulo2");
-let rotulo1 = document.getElementById ("rotulo3");
-let rotulo2 = document.getElementById ("rotulo4");
 
 
-rotulo1?.innerHTML = "Tiempo de la Vuelta Número 1";
-rotulo2?.innerHTML = "Tiempo de la Vuelta Número 2";
-rotulo3?.innerHTML = "Tiempo de la Vuelta Número 3";
-rotulo4?.innerHTML = "Tiempo de la Vuelta Número 4";
+
+rotulo1?.innerHTML = "Ingrese el Precio del producto";
+rotulo2?.innerHTML = "Ingrese el descuento a aplicar";
+
 
 btnEnv?.addEventListener ("click", () => {
-let vuelta1: number = Number(dato1.value);
-let vuelta2: number = Number(dato2.value);
-let vuelta3: number = Number(dato3.value);
-let vuelta4: number = Number(dato4.value);
+let precio: number = Number(dato1.value);
+let porcentaje: number = Number(dato2.value);
 
-let tiempototal: number = vuelta1 + vuelta2 + vuelta3 + vuelta4;
+let descuento: number = precio*porcentaje/100;
 
-let promediodevuelta: number = tiempototal/4;
+let preciototal: number = precio - descuento
 
-console.log("El tiempo total de las vueltas es" tiempototal);
-console.log("El promedio total de vueltas es de" promediodevuelta);
+console.log("El total de su compra es" preciototal);
+
 
 });
+
 
---
+++ programacion/clase25deabril-daniel/tarea2.ts
@@ -0,0 +1,32 @@
+let dato1 = document.getElementById("dato1");
+let dato2 = document.getElementById("dato2");
+let dato3 = document.getElementById("dato3");
+let dato4 = document.getElementById("dato4");
+
+let btnEnv = document.getElementById ("btnEnviar");
+let rotulo1 = document.getElementById ("rotulo1");
+let rotulo2 = document.getElementById ("rotulo2");
+let rotulo3 = document.getElementById ("rotulo3");
+let rotulo4 = document.getElementById ("rotulo4");
+
+
+rotulo1?.innerHTML = "Tiempo de la Vuelta Número 1";
+rotulo2?.innerHTML = "Tiempo de la Vuelta Número 2";
+rotulo3?.innerHTML = "Tiempo de la Vuelta Número 3";
+rotulo4?.innerHTML = "Tiempo de la Vuelta Número 4";
+
+btnEnv?.addEventListener ("click", () => {
+let vuelta1: number = Number(dato1.value);
+let vuelta2: number = Number(dato2.value);
+let vuelta3: number = Number(dato3.value);
+let vuelta4: number = Number(dato4.value);
+
+let tiempototal: number = vuelta1 + vuelta2 + vuelta3 + vuelta4;
+
+let promediodevuelta: number = tiempototal/4;
+
+console.log("El tiempo total de las vueltas es" tiempototal);
+console.log("El promedio total de vueltas es de" promediodevuelta);
+
+});
+
