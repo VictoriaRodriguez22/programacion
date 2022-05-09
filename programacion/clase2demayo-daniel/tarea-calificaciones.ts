fbb4e4a64437ce47dbb90bd972aa88674137f1cf
VictoriaRodriguez22 <104107269+VictoriaRodriguez22@users.noreply.github.com>
2022-05-09 19:16:10 -0300

    tarea de calificaciones modifique la tarea de calificaciones.

--- programacion/clase2demayo-daniel/tarea-calificaciones.ts
+++ programacion/clase2demayo-daniel/tarea-calificaciones.ts
@@ -1,12 +1,17 @@
 //let nombredelAlumno: string (prompt("Ingrese el nombre del alumno:"));
-//let notapractica: string (prompt("Ingrese la nota práctica:"));
-//let notaproblemas: string (prompt("Ingrese la nota de los problemas:"));
-//let notateorica: string (prompt("Ingrese la nota de la teoria:"));
+//let notapractica: number (prompt("Ingrese la nota práctica:"));
+///let notaproblemas: number (prompt("Ingrese la nota de los problemas:"));
+//let notateorica: number (prompt("Ingrese la nota de la teoria:"));
 
 let notafinal: number;
 notafinal = 0;
 let nota: number;
 nota = 0;
+notapractica = 0;
+notaproblemas = 0;
+notateorica = 0;
+
+if (nombredelAlumno != ""){
 
 while ( 0<notapractica<10 ){
   notapractica = notapractica*0.1;
@@ -17,7 +22,8 @@ while (0<notaproblemas>10){
  while (0<notateorica<10) {
 notateorica = notateorica*0.4;
 }
-
+}
 nota= notapractica + notaproblemas + notateorica;
 notafinal= nota / 3;
 console.log("La nota final de" nombredelAlumno "es de:" notafinal)
+
