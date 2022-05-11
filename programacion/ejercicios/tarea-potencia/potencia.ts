8e972f58a847e16ea551a010fbc5def1c22cd7b1
VictoriaRodriguez22 <104107269+VictoriaRodriguez22@users.noreply.github.com>
2022-05-11 19:34:23 -0300

    funtion-potencia hice la funcion de potencia

--- programacion/ejercicios/tarea-potencia/potencia.ts
+++ programacion/ejercicios/tarea-potencia/potencia.ts
@@ -5,28 +5,28 @@ let rotulo1 = document.getElementById("rotulo1");
 let rotulo2 = document.getElementById("rotulo2");
 let btnEnviar = document.getElementById("btnEnviar");
 
+let contador: number;
+let resultado: number = 1;
+
 rotulo1.innerHTML = "Ingrese la Base";
-rotulo2.innerHTML = "Ingrese el exponenete";
+rotulo2.innerHTML = "Ingrese el exponente";
 
-btnEnviar.addEventListener ("clcik", () => {
-  let base : number = Number(dato1.value);
-  let exponente : number = Number(dato2.value);
-  let contador : number;
-  let resultado : number;
+btnEnviar.addEventListener("click", () => {
+  let potenciadeunNumero = () => {
+    let base: number = Number(dato1.value);
+    let exponente: number = Number(dato2.value);
 
-if (exponente= 0){
-   
-  resultado = 1;
-
-}else if (exponente>0){
-
-  for(contador = 1, contador => exponente, contador++){
-    base = base*base
-    resultado = base
-  }
-}else {
- console.log("El exponente es menor que 0, intente con otro valor");
-}
- })
-
-console.log("La potencia es", resultado);
+    if (exponente === 0) {
+      resultado = 1;
+      console.log("la potencia es", resultado);
+    } else if (exponente > 0) {
+      for (contador = 1; contador <= exponente; contador++) {
+        resultado = resultado * base;
+      }
+      console.log("La potencia es", resultado);
+    } else {
+      console.log("El exponente es menor que 0, intente con otro valor");
+    }
+  };
+  potenciadeunNumero();
+});
